d0d30e553004f7853a8a1da9a573b549e82a76fc
rickkcastro <Heinrickoficial25@gmail.com>
2025-06-08 22:36:42 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-100">
-      <h1 className="text-4xl font-bold">Hello World!</h1>
-      <p className="mt-4 text-center text-lg text-green-100">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quidem
-        reprehenderit porro eaque! Consequuntur officia quos consequatur rem
-        officiis nihil quaerat, at, tempore laboriosam animi voluptatibus ab
-        voluptate! Accusantium, blanditiis.
-      </p>
-    </div>
-  );
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+  return <Button>Hello World!</Button>;
 }
